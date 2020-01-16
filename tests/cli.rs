9693f57d238e77d8401f97329d0d682061b860f4
Ondřej Čertík <ondrej@certik.us>
2020-01-16 16:04:51 -0700

    Revert "Don't run cmake"
    This reverts commit e9d32acbbdca962bfd4a3d07e4969274aef29f16.

--- tests/cli.rs
+++ tests/cli.rs
@@ -83,22 +83,39 @@ fn test_help4() {
             predicate::str::contains("--help       Prints help information"));
 }
 
+
 #[test]
-fn test_help5() {
-    let mut cmd = Command::cargo_bin("fpm").unwrap();
-    cmd.arg("--help");
-    cmd.assert()
+fn test_1() {
+    let mut build = Command::cargo_bin("fpm").unwrap();
+    build.arg("build")
+        .current_dir("tests/1");
+    build.assert()
         .success2()
-        .stdout(
-            predicate::str::contains("--help       Prints help information"));
+        .stdout(predicate::str::contains("Built target p1")
+                .and(predicate::str::contains("TEST1 OK").not()));
+
+    let mut run = Command::cargo_bin("fpm").unwrap();
+    run.arg("run")
+        .current_dir("tests/1");
+    run.assert()
+        .success2()
+        .stdout(predicate::str::contains("TEST1 OK"));
 }
 
 #[test]
-fn test_help6() {
-    let mut cmd = Command::cargo_bin("fpm").unwrap();
-    cmd.arg("--help");
-    cmd.assert()
+fn test_2() {
+    let mut build = Command::cargo_bin("fpm").unwrap();
+    build.arg("build")
+        .current_dir("tests/2");
+    build.assert()
         .success2()
-        .stdout(
-            predicate::str::contains("--help       Prints help information"));
+        .stdout(predicate::str::contains("Built target p1")
+                .and(predicate::str::contains("TEST2 OK").not()));
+
+    let mut run = Command::cargo_bin("fpm").unwrap();
+    run.arg("run")
+        .current_dir("tests/2");
+    run.assert()
+        .success2()
+        .stdout(predicate::str::contains("TEST2 OK"));
 }
