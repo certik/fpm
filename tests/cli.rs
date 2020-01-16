79562f7d9474f9053dbf81138b4d1bdbd4dfaefa
Ondřej Čertík <ondrej@certik.us>
2020-01-16 13:48:01 -0700

    Revert "Do not run cmake"
    This reverts commit 61f530379774574cf8f0f267ff453cc9e83e76d5.

--- tests/cli.rs
+++ tests/cli.rs
@@ -52,3 +52,40 @@ fn test_help() {
         .stdout(
             predicate::str::contains("--help       Prints help information"));
 }
+
+
+#[test]
+fn test_1() {
+    let mut build = Command::cargo_bin("fpm").unwrap();
+    build.arg("build")
+        .current_dir("tests/1");
+    build.assert()
+        .success2()
+        .stdout(predicate::str::contains("Built target p1")
+                .and(predicate::str::contains("TEST1 OK").not()));
+
+    let mut run = Command::cargo_bin("fpm").unwrap();
+    run.arg("run")
+        .current_dir("tests/1");
+    run.assert()
+        .success2()
+        .stdout(predicate::str::contains("TEST1 OK"));
+}
+
+#[test]
+fn test_2() {
+    let mut build = Command::cargo_bin("fpm").unwrap();
+    build.arg("build")
+        .current_dir("tests/2");
+    build.assert()
+        .success2()
+        .stdout(predicate::str::contains("Built target p1")
+                .and(predicate::str::contains("TEST2 OK").not()));
+
+    let mut run = Command::cargo_bin("fpm").unwrap();
+    run.arg("run")
+        .current_dir("tests/2");
+    run.assert()
+        .success2()
+        .stdout(predicate::str::contains("TEST2 OK"));
+}
