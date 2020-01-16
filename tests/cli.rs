6b60b7907cfe3af365a6f752f26349981a31c347
Ondřej Čertík <ondrej@certik.us>
2020-01-16 13:37:05 -0700

    Simplify the Rust project

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,54 +1,11 @@
-use std::process::Command;  // Run programs
-use assert_cmd::prelude::*; // Add methods on commands
-use predicates::prelude::*; // Used for writing assertions
-use assert_cmd::assert::Assert;
-#[cfg(unix)]
-use std::os::unix::process::ExitStatusExt;
-use std::process::ExitStatus;
-
-pub trait Success2 {
-    // Our own function with better reporting of errors
-    fn success2(self) -> Self;
-}
-
-#[cfg(unix)]
-fn get_signal(status: ExitStatus) -> Option<i32> {
-    status.signal()
-}
-
-#[cfg(not(unix))]
-fn get_signal(_status: ExitStatus) -> Option<i32> {
-    None
-}
-
-impl Success2 for Assert {
-    fn success2(self) -> Self {
-        if !self.get_output().status.success() {
-            let output = self.get_output();
-            let code = output.status.code();
-            println!("status: {}", output.status);
-            println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-            println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-            if cfg!(unix) {
-                if code.is_none() {
-                    let signal = get_signal(output.status).unwrap();
-                    panic!("INTERRUPTED with signal: {}", signal);
-                }
-            }
-            let actual_code = code.unwrap();
-            println!("code: {}", actual_code);
-            panic!("Non zero exit code");
-        }
-        self
-    }
-}
-
 #[test]
-fn test_help() {
-    let mut cmd = Command::cargo_bin("fpm").unwrap();
-    cmd.arg("--help");
-    cmd.assert()
-        .success2()
-        .stdout(
-            predicate::str::contains("--help       Prints help information"));
+fn test_run() {
+    let output = std::process::Command::new("target/debug/fpm")
+                                        .output().unwrap();
+    println!("status: {}", output.status);
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    if !output.status.success() {
+        panic!("Command failed.")
+    }
 }
